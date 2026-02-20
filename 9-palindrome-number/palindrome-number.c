484275f6a199826f50ed7cdfd1fc0c98a5a9133b
RUBIKA T <thangavelthangavel578129@gmail.com>
2026-02-20 11:25:22 +0530

    Time: 0 ms (100.00%) | Memory: 8.4 MB (16.13%) - LeetSync

---
+++ 9-palindrome-number/palindrome-number.c
@@ -0,0 +1,18 @@
+bool isPalindrome(int x) {
+    int ori=x;
+    long rev=0;
+    if(x<0){
+        return false;
+    }
+    while(x!=0){
+        int digit=x%10;
+        rev=rev*10+digit;
+        x=x/10;
+    }
+    if(ori==rev){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
